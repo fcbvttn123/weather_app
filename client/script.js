e3c157b76e5ca4e9759227e22ef0ac252e515e55
ngan <fcbvttn@gmail.com>
2026-01-17 16:27:20 -0500

    download cors, fix bug importing axios

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import { axios } from "axios";
+import axios from "axios";
 
 // get user location
 navigator.geolocation.getCurrentPosition(getPositionSuccess, getPositionError);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/weather", (request, response) => {
