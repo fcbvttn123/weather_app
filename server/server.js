b216dc44cbe1dd157579554884bc5224bcdad633
ngan <fcbvttn@gmail.com>
2026-01-17 17:44:00 -0500

    make a request to openweathermap

--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/weather", (request, response) => {
   const { lat, lon } = request.query;
-  console.log(lat, lon);
-  response.send();
+  axios
+    .get("https://api.openweathermap.org/data/3.0/onecall", {
+      params: {
+        appid: process.env.API_KEY,
+        lat,
+        lon,
+        units: "imperial",
+        exclude: "minutely, alerts",
+      },
+    })
+    .then(({ data }) => {
+      response.json(data);
+    })
+    .catch((err) => {
+      response.sendStatus(500);
+      response.json({
+        error: err.response.data,
+        message: err.message,
+      });
+    });
 });
 
 app.listen(3001);
